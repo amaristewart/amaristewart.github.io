111ed16dec1f141731079821361ab64634fe158e
amaristewart <151667406+amaristewart@users.noreply.github.com>
2024-12-24 23:42:45 -0500

    Add trailer

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Hover effect
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 document.querySelector("h2").onmouseover = event => {
@@ -18,4 +19,39 @@ document.querySelector("h2").onmouseover = event => {
     
     iterations += 1 / 3;
   }, 60);
+}
+
+// Trailer effect
+const trailer = document.getElementById("trailer");
+
+const animateTrailer = (e, interacting) => {
+    const x = e.clientX - trailer.offsetWidth / 2,
+          y = e.clientY - trailer.offsetHeight / 2;
+    
+    const keyframes = {
+        transform: `translate(${x}px, ${y}px) scale ${interacting ? 1.5 : 1}`
+    }
+
+    trailer.animate(keyframes, {
+        duration: 800,
+        fill: "forwards"
+    });
+}
+
+const getTrailerClass = type => {
+    switch(type) {
+        case "video":
+            return "fa-solid fa-play";
+        default:
+            return "fa-solid fa-ellipsis";
+    }
+}
+
+window.onmousemove = e => {
+    const interactable = e.target.closest(".interactable");
+          interacting = interactable !== null;
+
+    const icon = document.getElementById("trailer-icon");
+
+    animateTrailer(e, interacting);
 }
