80ca941f0e51e93308e05d16e4a50a45fc6acdb1
amaristewart <151667406+amaristewart@users.noreply.github.com>
2025-01-01 14:16:28 -0500

    Add cards

--- script.js
+++ script.js
@@ -64,6 +64,18 @@ document.addEventListener("DOMContentLoaded", () => {
                     }
                 });
             });
+
+        // Card effect
+        document.getElementById("cards").onmousemove = e => {
+          for(const card of document.getElementsByClassName("card")) {
+            const rect = card.getBoundingClientRect(),
+                  x = e.clientX - rect.left,
+                  y = e.clientY - rect.top;
+        
+            card.style.setProperty("--mouse-x", `${x}px`);
+            card.style.setProperty("--mouse-y", `${y}px`);
+          };
+        }
   });
     }
 
