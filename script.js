a505c2fc198b83d44fe1c06ff5c686ec68baa4ca
amaristewart <151667406+amaristewart@users.noreply.github.com>
2025-01-03 14:05:50 -0500

    Remove trailer

--- script.js
+++ script.js
@@ -79,33 +79,4 @@ document.addEventListener("DOMContentLoaded", () => {
         }
   });
     }
-
-    // Trailer effect
-    const trailer = document.getElementById("trailer");
-
-    if (trailer) {
-        const animateTrailer = (e, interacting) => {
-            const x = e.clientX - trailer.offsetWidth / 2,
-                  y = e.clientY - trailer.offsetHeight / 2;
-            
-            const keyframes = {
-                transform: `translate(${x}px, ${y}px) scale(${interacting ? 1.5 : 1})`
-            }
-
-            trailer.animate(keyframes, {
-                duration: 800,
-                fill: "forwards"
-            });
-        }
-
-        window.onmousemove = e => {
-            const interactable = e.target.closest(".interactable");
-            const interacting = interactable !== null;
-            const icon = document.getElementById("trailer-icon");
-
-            animateTrailer(e, interacting);
-        }
-    } else {
-        console.error("Trailer element not found!");
-    }
 });
